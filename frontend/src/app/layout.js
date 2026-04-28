a31966fc338e173efaf27b735007be645ee0b49a
propranjal-github <pranjalpathak214@gmail.com>
2026-04-29 00:51:59 +0530

    fresh clean commit

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "JEEAce",
+  description: "JEEAce - Your Ultimate JEE Preparation Companion",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      
+      <body suppressHydrationWarning={true}>
+        {children}
+      </body>
+    </html>
+  );
+}
